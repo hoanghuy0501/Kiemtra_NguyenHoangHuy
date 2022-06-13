37660c666af368e1e0109a81805aea0d46ac49aa
huy <huy@DESKTOP-2M8RCD5>
2022-06-13 13:25:26 +0700

    Add project files.

---
+++ Kiemtra_NguyenHoangHuy/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Kiemtra_NguyenHoangHuy.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Kiemtra_NguyenHoangHuy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
